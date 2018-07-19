0bfe3df504b26af525cf2e6bbf360624ffe85164
Jimmy Huang <jimmy2002916@hotmail.com>
2018-07-19 13:14:52 +0800

    Test single XGB, score 2.00.

--- prediction.py
+++ prediction.py
@@ -1,8 +1,11 @@
 # coding: utf-8
 import pandas as pd
 
-train = pd.read_csv("data/train.csv")
-test = pd.read_csv("data/test.csv")
+train_original = pd.read_csv("data/train.csv")
+test_original = pd.read_csv("data/test.csv")
+
+train = train_original
+test = test_original
 
 label = 'target' #label
 features = list(set(train.columns) - {label,'ID'})
@@ -10,20 +13,27 @@ features = list(set(train.columns) - {label,'ID'})
 X = train[features]
 y = train[label]
 
+#for xgb 
+X = X.as_matrix()
+y = y.as_matrix()
+
 #%%
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # In[21]:
 # Feature Scaling
+print("Feature scaling...")
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 # In[22]:
-from sklearn.tree import DecisionTreeRegressor
-model = DecisionTreeRegressor()
+print("training...")
+### Change the File output name into the model you use here. 
+from xgboost import XGBRegressor
+model = XGBRegressor()            
 model.fit(X, y)
 #y_pred = model.predict(X_test)
 
@@ -34,13 +44,18 @@ fpr, tpr, thresholds = metrics.roc_curve(y_test, y_pred, pos_label=2)
 auc = metrics.auc(fpr, tpr)
 """
 # In[24]:
+print("predicting... ")
 features_for_test = list(set(test.columns) - {'ID'})
-final_prediction = model.predict(test[features_for_test])
+test = test[features_for_test]
+test = test.as_matrix()
+final_prediction = model.predict(test)
 
 # In[26]:
+print("Outputing file...")
+test = test_original
 ID = test['ID']
 target = pd.DataFrame(final_prediction, columns = ['target'])
 target[target < 0] = 0
 result = pd.concat([ID, target], axis=1)
-result.to_csv("submission_DT.csv", index = False)
+result.to_csv("submission_xgb.csv", index = False)
 
